933f9757f92d5e93dc81a75eb18bb865f1d97b1f
Max-Gabriel-Susman <gabe.susman@gmail.com>
2026-08-04 19:45:17 -0600

    introduced a new safety_controller component

---
+++ safety_controller/main.c
@@ -0,0 +1,16 @@
+#include "xil_printf.h"
+
+int main(void)
+{
+    xil_printf("Initializing Argus Safety Controller...\r\n");
+
+    // Initialization process
+    
+    xil_printf("Argus Safety Controller initialized.\r\n");
+
+    while (1) {
+        // stay here
+    }
+
+    return 0;
+}
