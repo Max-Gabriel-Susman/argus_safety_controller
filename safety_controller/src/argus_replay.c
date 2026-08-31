323b3b3f9493ae16dbfb6240084744c42e4c81de
Max-Gabriel Susman <gabe.susman@gmail.com>
2026-08-31 06:24:04 -0600

    included argus_replay header in argus_replay.c

---
+++ safety_controller/src/argus_replay.c
@@ -0,0 +1,14 @@
+
+// Needs impl.
+
+#include "argus_replay.h"
+
+// Needs impl.
+
+/* Relay host. Same machine as the telemetry destination in
+ * argus_net.c; if that address moves, both must move together. */
+#define ARGUS_RELAY_IP_A 192
+#define ARGUS_RELAY_IP_B 168
+#define ARGUS_RELAY_IP_C 1
+#define ARGUS_RELAY_IP_D 20
+// Needs imple.
